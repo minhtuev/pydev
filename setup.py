0e56d14b0f04ba98a3eefbc182f4360f90b98d62
Nickola <nickola@nickola.ru>
2012-08-24 19:58:42 +0400

    first commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name='pydev',
       version='1.0',
-      packages=find_packages(),
+      packages=find_packages(exclude=['ez_setup', 'tests*']),
       entry_points={
           'console_scripts': ['pydev = pydev:execute_from_command_line'],
       }
